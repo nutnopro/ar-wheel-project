4e421ad3ff25d71b5551492f15411993477290b4
Tkp-7663 <tharakorn.pangporn2547@gnail.com>
2026-03-01 21:32:56 +0700

    chore: update dependencies in package.json
    - Added axios and react-hook-form to dependencies
    - Updated @react-native-community/cli and related packages to version 20.1.2
    - Added react-native-vector-icons to dependencies

--- android/app/src/main/java/com/arwheelapp/MainApplication.kt
+++ android/app/src/main/java/com/arwheelapp/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.arwheelapp.modules.ARLauncherPackage
 
 class MainApplication : Application(), ReactApplication {
 
